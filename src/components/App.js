0a068e07975824fc876f52289985564ed76d1705
Erik Rasmussen <rasmussenerik@gmail.com>
2020-02-03 15:28:45 -0500

    Netlify link

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import styles from './App.scss'
-import Home from './Home'
-import Nav from './Nav'
-import Breadcrumbs from './Breadcrumbs'
-import TwitterFollowButton from './TwitterFollowButton'
-import cx from 'classnames'
+import React from "react";
+import styles from "./App.scss";
+import Home from "./Home";
+import Nav from "./Nav";
+import Breadcrumbs from "./Breadcrumbs";
+import TwitterFollowButton from "./TwitterFollowButton";
+import cx from "classnames";
 
 const App = ({ children, path, version, breadcrumbs }) => {
-  const isHome = path === '/'
-  const url = `https://redux-form.com/${version}`
+  const isHome = path === "/";
+  const url = `https://redux-form.com/${version}`;
   return (
     <div className={cx(styles.app, { [styles.hasNav]: !isHome })}>
       {!isHome && <Nav path={path} url={url} />}
       <div className={styles.triplebyte}>
-        💰{' '}
+        💰{" "}
         <strong>
-          Wanna get paid the big bucks writing React?{' '}
+          Wanna get paid the big bucks writing React?{" "}
           <a href="https://triplebyte.com/a/V6j0KPS/rfcom" target="_blank">
             Take this quiz
-          </a>{' '}
+          </a>{" "}
           and get offers from top tech companies!
-        </strong>{' '}
+        </strong>{" "}
         💰
       </div>
       <div className={styles.contentAndFooter}>
@@ -71,10 +71,15 @@ const App = ({ children, path, version, breadcrumbs }) => {
             <TwitterFollowButton username="erikras" showUsername large />
             <TwitterFollowButton username="ReduxForm" showUsername large />
           </div>
+          <div>
+            <a href="https://www.netlify.com" target="_blank">
+              <img src="https://www.netlify.com/img/global/badges/netlify-color-bg.svg" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
